54d4a0219e2f6957390dc20a10e68ea10d21e26e
zzFon <735239697@qq.com>
2020-05-20 16:47:31 +0800

    2020-05-20 edition

--- simulation/SpectrumAnalysis.m
+++ simulation/SpectrumAnalysis.m
@@ -2,16 +2,17 @@
 
 
 for i = 1:6
-    file_name = strcat('dataset_horn',num2str(i));
-    file_name = strcat(file_name,'.wav');
-    fprintf('reading %s...\n',file_name);
-    [y,fs] = audioread(file_name);   
-    sz = size(y);
-    if sz(2) > 1
-        gun = ((y(:,1)+y(:,2))/2)'; % 多声道，只要一个
-    elseif sz(1) == 1
-        gun = (y(:,1))'; % 单声道
-    end
+%     file_name = strcat('dataset_horn',num2str(i));
+%     file_name = strcat(file_name,'.wav');
+%     fprintf('reading %s...\n',file_name);
+%     [y,fs] = audioread(file_name);   
+%     sz = size(y);
+%     if sz(2) > 1
+%         gun = ((y(:,1)+y(:,2))/2)'; % 多声道，只要一个
+%     elseif sz(1) == 1
+%         gun = (y(:,1))'; % 单声道
+%     end
+    gun = data_gun(1,:);
     
     %figure(i);
     figure(1);
